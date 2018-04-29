2016d0591c0d75933ea73cba361691340f33b4a3
DakotaConnors <djconnors@mavs.coloradomesa.edu>
2018-04-29 14:43:23 -0600

    Add files via upload

---
+++ 2nd Assignment/DepthLimitAgent.py
@@ -0,0 +1,86 @@
+import random
+from Const import Const
+from Move import Move
+from Game import Game
+from functools import lru_cache
+
+class DepthLimitAgent:
+    def __init__(self, side, maxDepth):
+        if side != Const.MARK_O and side != Const.MARK_X:
+            raise ValueError("side must be MARK_X or MARK_O")
+        self._side = side
+        self.maxDepth = maxDepth
+
+
+    def Value(self,game, maxDepth, currentDepth):
+        currentDepth = currentDepth + 1
+        #print(currentDepth)
+        ans = None
+        state = game.getState()
+        if state == Const.STATE_WIN_O:
+            if self._side == Const.MARK_O: ans = 1
+            else: ans = -1
+        elif state == Const.STATE_WIN_X:
+            if self._side == Const.MARK_X: ans = 1
+            else: ans = -1
+        elif state == Const.STATE_DRAW:
+            ans = 0
+
+        if ans != None: return (ans,0)
+
+        iside = 0
+        if self._side == Const.MARK_O: iside = 1
+        else: iside = -1
+
+        iturn = 0
+        if state == Const.STATE_TURN_O: iturn = 1
+        else: iturn = -1
+
+        myTurn = (iside == iturn)
+        myOptions = 0
+
+        if currentDepth == self.maxDepth:
+            #pass
+            print('Hit the depth limit, not sure about move')
+            #Currently if it hits the depth limit the agent makes the move a '0'
+            return 0,0
+        for move in game.getMoves():
+            move.play(game)
+            (moveValue,moveOptions)=self.moveValue(game, currentDepth)
+            move.unplay(game)
+            myOptions = myOptions + 1 + moveOptions
+            if ans == None:
+                ans = moveValue
+            else:
+                if myTurn:
+                   ans = max(ans,moveValue)
+                else:
+                   ans = min(ans,moveValue)
+        #print ('This move is', ans)
+        return (ans,myOptions)
+
+    def moveValue(self,game,depth):
+        ans = self.Value(game, self.maxDepth, depth)
+        return ans
+
+    def move(self,game):
+        (maxValue,maxOptions)=self.moveValue(game, 0)
+        #print('value = ' + str(self.moveValue(game)))
+        playable = []
+        maxPlayableOption = 0
+        for move in game.getMoves():
+            move.play(game)
+            (moveValue,moveOptions)=self.moveValue(game, 0)
+            move.unplay(game)
+            if moveValue == maxValue:
+                playable.append((move,moveOptions))
+                maxPlayableOption = max(maxPlayableOption,moveOptions)
+
+        bestPlayable = []
+        for (move,options) in playable:
+            if options == maxPlayableOption:
+                bestPlayable.append(move)
+
+        #print(len(bestPlayable))
+        spot=random.randint(0,len(bestPlayable)-1)
+        return bestPlayable[spot]
---
+++ 2nd Assignment/README.md
@@ -0,0 +1,34 @@
+DepthLimitAgent.py is the MinMax agent with a depth Limit
+
+
+class DepthLimitAgent:
+
+  def __init__(self, side, maxDepth):
+    if side != Const.MARK_O and side != Const.MARK_X:
+
+      raise ValueError("side must be MARK_X or MARK_O")
+        
+    self._side = side
+        
+    self.maxDepth = maxDepth
+
+Max depth allowed is stored in the agent Class
+
+
+
+The current depth of the search is passed as a parameter:
+
+def moveValue(self,game,depth):
+
+def Value(self,game, maxDepth, currentDepth):
+ 
+  currentDepth = currentDepth + 1
+
+
+
+Finally when the max depth is reached it returns a 0 to signal it's unsure how good of a move it is
+
+if currentDepth == self.maxDepth:
+  print('Hit the depth limit, not sure about move')
+  #Currently if it hits the depth limit the agent makes the move a '0'
+  return 0,0
